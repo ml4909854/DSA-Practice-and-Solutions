38283d684a1c99d5f6d70d998e3c045fe7b41468
Mahesh Kumar <ml4909854@gmail.com>
2024-12-26 13:32:49 +0530

    Dsa

--- Javascript/problem6.js
+++ Javascript/problem6.js
@@ -1,6 +1,5 @@
 
-        for (let i = 0; i < 5; i++) {
-            let bag = ""
+p            let bag = ""
             for (let j = 0; j <= i; j++) {
                 bag = bag + "+ "
             }
