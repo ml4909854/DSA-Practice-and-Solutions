be782b6394d0e29afd0b6027486aa8f6e3061c52
Mahesh Kumar <ml4909854@gmail.com>
2025-08-02 14:03:06 +0530

    dsa

--- LeetCode/index.js
+++ LeetCode/index.js
@@ -16,5 +16,84 @@ var isPalindrome = function(x) {
     }
 };
 
-let x = 550
-isPalindrome(x)
+// let x = 550
+// isPalindrome(x)
+
+var twoSum = function(nums, target) {
+    let bag = ""
+    for(let i = 0 ; i<nums.length; i++){
+        for(let j = i+1 ; j<nums.length-1 ; j++){
+            if(nums[i] + nums[j] === target){
+            bag += i+ " " +j
+               }
+        }
+    }
+    let result = bag.trim().split(" ").map(Number)
+    return(result)
+
+};
+
+// let nums = [2 ,7 , 11 , 10]
+// let target = 9
+// twoSum(nums , target)
+
+
+
+var detectCapitalUse = function(word) {
+    for(let i = 0 ; i<word.length ; i++){
+           let str = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
+       let result = word[i] === word[i].toUpperCase()
+       if(!result){
+        console.log(false)
+        break
+       }
+    }
+    console.log(true)
+};
+let word = "FlaG"
+// detectCapitalUse(word)
+
+
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+
+var romanToInt = function(s) {
+    let obj  = {
+        I:1,
+        II:2,
+        III:3,
+        IV:4,
+        V:5,
+        VI:6,
+        Vi
+
+
+    }
+};
+let str = "III"
+// romanToInt(str)
+
+
+var longestCommonPrefix = function(strs) {
+    console.log(strs)
+    for(let i =  0 ; i<strs.length ; i++){
+        let bag =  ''
+      for(let j = 0 ; j<strs[i].length ; j++){
+
+          bag += strs[i][j]
+          
+      }
+      console.log(bag)
+    }
+};
+
+
+// Input: strs = ["flower","flow","flight"]
+let strs = ["flower","flow","flight"]
+longestCommonPrefix(strs)
+// Output: "fl"
